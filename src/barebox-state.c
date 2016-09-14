hexagon-geo-surv/dt-utils

commit feb64890a302565974abf5178284965e788c698c

barebox-state: fall back to /state if there is no alias defined
state_get() already supports this if no state name is given. This just
removes the default state name to make use of this feature.

Without this, an alias named 'state' is needed for barebox-state work
without specifying the state node.

Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

src/barebox-state.c
@@ -429,7 +429,6 @@ int main(int argc, char *argv[])
 	int lock_fd;
 	int nr_states = 0;
 	bool readonly = true;
-	const char *default_state = "state";
 
 	INIT_LIST_HEAD(&sg_list);
 	INIT_LIST_HEAD(&state_list.list);
@@ -488,7 +487,7 @@ int main(int argc, char *argv[])
 		struct state_list *new_state;
 
 		new_state = xzalloc(sizeof(*new_state));
-		new_state->name = default_state;
+		new_state->name = NULL;
 
 		list_add_tail(&new_state->list, &state_list.list);
 		++nr_states;

commit d4c997a3016a18033546766e9562baa1344cb981

barebox-state: make sure state->name is valid
It's used later while setting state variables.

Fixes: feb64890a302 ("barebox-state: fall back to /state if there is no alias defined")
Signed-off-by: Michael Olbrich <m.olbrich@pengutronix.de>

src/barebox-state.c
@@ -508,6 +508,8 @@ int main(int argc, char *argv[])
 
 	list_for_each_entry(state, &state_list.list, list) {
 		state->state = state_get(state->name, readonly);
+		if (!state->name)
+			state->name = state->state->name;
 		if (IS_ERR(state->state)) {
 			ret = 1;
 			goto out_unlock;