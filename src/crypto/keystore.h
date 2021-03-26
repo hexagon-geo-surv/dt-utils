50f7f71f81da2b2544098c7d4a83f7f894503ebb
Roland Hieber <rhi@pengutronix.de>
2021-03-26 20:28:22 +0100

    treewide: remove references to CREDITS file
    We have no such file, and barebox has also gotten rid of it since the
    code was copied from there.
    
    Link: https://git.pengutronix.de/cgit/barebox/commit/?id=cd24f633a063f0fd2c18
    Signed-off-by: Roland Hieber <rhi@pengutronix.de>

--- src/crypto/keystore.h
+++ src/crypto/keystore.h
@@ -1,9 +1,6 @@
 /*
  * (C) Copyright 2015 Pengutronix, Marc Kleine-Budde <kernel@pengutronix.de>
  *
- * See file CREDITS for list of people who contributed to this
- * project.
- *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
  * published by the Free Software Foundation; version 2 of
