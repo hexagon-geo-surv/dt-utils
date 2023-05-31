e5c524875a919c118428ea2f1318707450fde46a
Ahmad Fatoum <a.fatoum@pengutronix.de>
2023-05-31 17:31:23 +0200

    meson: add simple integration test
    It's straight forward to use meson as test runner, so let's compile a
    very simple test application that links against libdt-utils and verifies
    crc32 behaves as one would expect.
    
    Tested-by: Roland Hieber <rhi@pengutronix.de>
    Link: https://lore.pengutronix.de/oss-tools/20230531153125.1408092-4-a.fatoum@pengutronix.de
    Signed-off-by: Ahmad Fatoum <a.fatoum@pengutronix.de>

---
+++ test/crc32.c
@@ -0,0 +1,18 @@
+#include <stdint.h>
+#include <assert.h>
+
+#include <dt/common.h>
+
+int main(void)
+{
+	const char *str = "Hello, World!";
+	uint32_t checksum;
+
+	checksum = crc32(0, str, strlen(str));
+	assert(checksum == 0xec4ac3d0);
+
+	checksum = crc32_no_comp(0, str, strlen(str));
+	assert(checksum == 0xe33e8552);
+
+	return 0;
+}
